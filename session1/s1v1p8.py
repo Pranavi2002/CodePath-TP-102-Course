b35e03613da503796195718dbe6bc5ddf6761f9f
KOLIPAKA V N S SAI PRANAVI <66314704+Pranavi2002@users.noreply.github.com>
2025-06-04 14:11:40 -0400

    Update s1v1p8.py

--- session1/s1v1p8.py
+++ session1/s1v1p8.py
@@ -1,7 +1,11 @@
 def print_todo_list(task):
     print("Pooh's To Dos:")
-    for i in range(len(task)):
-        print(f"{i+1}. {task[i]}")
+
+    # for i in range(len(task)):
+    #     print(f"{i+1}. {task[i]}")
+
+    for index, string in enumerate(task, start=1):
+        print(f"{index}.{string}")
 
 task = ["Count all the bees in the hive", "Chase all the clouds from the sky", "Think", "Stoutness Exercises"]
 print_todo_list(task)
